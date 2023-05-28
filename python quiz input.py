f3fbe67644c6ab81dd2479fabab614f9a4308148
Samreen Razzaq <96229701+SamreenRazzaq@users.noreply.github.com>
2023-05-28 19:37:40 +0500

    Add files via upload

---
+++ python quiz input.py
@@ -0,0 +1,13 @@
+bill=int(input("enter total bill"))
+amount=int(input("amount paid by customer"))
+back=amount-bill
+print("amount to be given back to customer",back)
+x=[500,50,20,10]
+print("x",[500,50,20,10])
+if(bill>amount):
+    print("pay more and more")
+else:
+    print("thank you")
+    
+
+    
